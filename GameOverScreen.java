e73584976c1599b677b5392ad0bc7ce7a0d467a8
fquiroga01 <142179103+fquiroga01@users.noreply.github.com>
2024-05-26 12:13:47 -0400

    Create GameOverScreen.java

---
+++ GameOverScreen.java
@@ -0,0 +1,89 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class GameOverScreen implements Screen {
+	private final GameLluviaMenu game;
+	private SpriteBatch batch;	   
+	private BitmapFont font;
+	private OrthographicCamera camera;
+	private Texture backgroundTexture;
+	private Texture gameOverTexture;
+
+	public GameOverScreen(final GameLluviaMenu game) {
+		this.game = game;
+        this.batch = game.getBatch();
+        this.font = game.getFont();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		this.backgroundTexture = new Texture(Gdx.files.internal("background.png"));
+		this.gameOverTexture = new Texture(Gdx.files.internal("gameover.png"));
+	}
+
+	@Override
+	public void render(float delta) {
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+
+		batch.begin();
+		
+		batch.draw(backgroundTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		
+		float scaleX = 0.32f; // Escala en el eje X
+		float scaleY = 0.32f; // Escala en el eje Y
+
+		batch.draw(gameOverTexture, 290, 215, gameOverTexture.getWidth() * scaleX, gameOverTexture.getHeight() * scaleY);
+		font.draw(batch, "Toca en cualquier lado para reiniciar.", 162, 172);
+		batch.end();
+
+		if (Gdx.input.isTouched()) {
+			game.setScreen(new GameScreen(game));
+			dispose();
+		}
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
