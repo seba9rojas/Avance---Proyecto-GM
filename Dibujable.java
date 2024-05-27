f7c78789a50168b310bb2c571bce68a2eb805955
Joako-C <142269210+Joako-C@users.noreply.github.com>
2024-05-26 21:27:56 -0400

    Delete Dibujable.java

--- Dibujable.java
+++
@@ -1,9 +0,0 @@
-package com.mygdx.game;
-
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-public interface Dibujable {
-    void dibujar(SpriteBatch batch);
-    void crear();
-    void destruir();
-}
