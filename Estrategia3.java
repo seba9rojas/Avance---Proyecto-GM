5ad76043c11128f4e63d6f6ee60d4187902a2dba
fquiroga01 <142179103+fquiroga01@users.noreply.github.com>
2024-06-29 21:53:56 -0400

    Create Estrategia3.java

---
+++ Estrategia3.java
@@ -0,0 +1,30 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.math.MathUtils;
+
+public class Estrategia3 implements TipoBalonStrategy {
+    @Override
+    public int determinarTipoBalon(int totalBalonesGenerados, int nivelActual) {
+        float probabilidadBalonesBuenos = 50;
+        float probabilidadBalonesMalos = 43;
+        float probabilidadBalonDorado = 5;
+        float probabilidadBalonHielo = 1;
+
+        for (int i = 0; i < nivelActual - 1; i++) {
+            probabilidadBalonesBuenos -= 2;
+            probabilidadBalonesMalos += 2;
+        }
+
+        float tipoBalonFloat = MathUtils.random(1, 100);
+        if (tipoBalonFloat <= probabilidadBalonesMalos)
+            return 1; // Balón Rojo
+        else if (tipoBalonFloat <= probabilidadBalonesMalos + probabilidadBalonesBuenos)
+            return 2; // Balón Normal
+        else if (tipoBalonFloat <= probabilidadBalonesMalos + probabilidadBalonesBuenos + probabilidadBalonDorado)
+            return 3; // Balón Dorado
+        else if (tipoBalonFloat <= probabilidadBalonesMalos + probabilidadBalonesBuenos + probabilidadBalonDorado + probabilidadBalonHielo)
+            return 4; // Balón Hielo
+        else
+            return 5; // Balón Veneno
+    }
+}
